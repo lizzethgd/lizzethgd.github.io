75f78c09ab16ac22b5d73a87c56d7429fcfdd21c
lizzethgd <lizzethgd@gmail.com>
2021-08-04 08:01:05 +0300

    deploy 17

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const path = require('path');
 const express = require("express");
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.static('public'))
+
+app.use(express.json())
+
 // Have Node serve the files for our built React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.use(cors());
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
@@ -15,7 +22,7 @@ app.get("/api", (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
 app.listen(PORT, () => {
